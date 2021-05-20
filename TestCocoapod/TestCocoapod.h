ae7d087f3efcc4510068ff36e5756211ed97bac4
Kiran P Nair <kiran@logiticks.com>
2021-05-20 11:22:01 +0530

    First Commit

---
+++ TestCocoapod/TestCocoapod.h
@@ -0,0 +1,18 @@
+//
+//  TestCocoapod.h
+//  TestCocoapod
+//
+//  Created by Moble Joseph on 12/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestCocoapod.
+FOUNDATION_EXPORT double TestCocoapodVersionNumber;
+
+//! Project version string for TestCocoapod.
+FOUNDATION_EXPORT const unsigned char TestCocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCocoapod/PublicHeader.h>
+
+
